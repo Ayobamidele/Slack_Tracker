d368026b70badda375cf743c2c33528e7a6bbee8
Ayobamidele Ewetuga <ayobamideleewetuga@gmail.com>
2023-09-07 15:33:58 +0100

    Final modifications being made

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from typing import Union
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
+import datetime
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"Hello": "World"}
+def get_time_and_day():
+    # Get the current UTC time
+    utc_now = datetime.datetime.utcnow()
+
+    # Get the day of the week
+    day_of_week = utc_now.strftime("%A")
+
+    # Format the time
+    utc_time = utc_now.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    # Return as a dictionary
+    return {"Day of Week": day_of_week, "UTC Time": utc_time}
 
 
 @app.get("/track/{track}/{slack_name}")
-def result(track: str, slack_name: str):
+def result(response: Response, track: str, slack_name: str):
+    # Get the result for time and day
+    time_and_day_result = get_time_and_day()
+    
+	# Get the current status code
+    current_status_code = response.status_code
+    
+	# Return as a dictionary
     return {
         "slack_name": f"{slack_name}",
-        "current_day": "Monday",
-        "utc_time": "2023-08-21T15:04:05Z",
-        "track": f"{track}",
-        "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-        "github_repo_url": "https://github.com/username/repo",
-        "status_code": 200,
+        "current_day": time_and_day_result["Day of Week"],
+        "utc_time": time_and_day_result["UTC Time"],
+        "track": f"{track.capitalize()}",
+        "github_file_url": "https://github.com/Ayobamidele/Slack_Tracker/blob/main/main.py",
+        "github_repo_url": "https://github.com/Ayobamidele/Slack_Tracker",
+        "status_code": current_status_code or 200,
     }
